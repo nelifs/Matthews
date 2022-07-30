41a98212d14125a75153a89b6a53bf8b88917522
nelifs <69263984+nelifs@users.noreply.github.com>
2022-07-30 23:49:29 +0300

    Delete src directory

--- src/sharding.js
+++
@@ -1,7 +0,0 @@
-const {ShardingManager} = require('discord.js');
-
-const manager = new ShardingManager('./src/index.js', {token: process.env.CLIENTTOKEN, totalShards: 30});
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
-void manager.spawn();
