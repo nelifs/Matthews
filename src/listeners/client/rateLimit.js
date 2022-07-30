d8d460e243772dda0553e38dccafad72ce0f2376
nelifs <69263984+nelifs@users.noreply.github.com>
2022-07-30 22:47:08 +0300

    Delete src directory

--- src/listeners/client/rateLimit.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (client, eventName, d) => {
-    await client.logger.warn('Main thread', d)
-}
--- src/structures/BaseListener.js
+++
@@ -1,9 +0,0 @@
-class BaseListener {
-    constructor(name, options = {}) {
-        this.name = name;
-        this.ws = options.ws || false;
-        this.event = options.event || null;
-    }
-}
-
-module.exports = BaseListener;
